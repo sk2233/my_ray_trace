9f0ded61c1b58295dec98c748bf7420018051a7b
shaokang.sk <shaokang@wepie.com>
2025-09-27 17:39:41 +0800

    测试 book2

--- book.go
+++ book.go
@@ -30,14 +30,16 @@ func TestBook() {
 	// 世界设置
 	background = mgl32.Vec3{0.5, 0.7, 1.0}
 	// 设置地面
-	world.Add(NewSphere(mgl32.Vec3{0, 10000, 600}, 10000, NewSolidLambert(mgl32.Vec3{0.5, 0.5, 0.5})))
+	chess := NewChessTexture(10, NewSolidColor(mgl32.Vec3{0.2, 0.3, 0.1}), NewSolidColor(mgl32.Vec3{0.9, 0.9, 0.9}))
+	world.Add(NewSphere(mgl32.Vec3{0, 10000, 600}, 10000, NewLambert(chess)))
 	// 设置其他场景
 	for i := -8; i < 8; i++ {
 		for j := -5; j < 5; j++ {
 			pos := mgl32.Vec3{float32(i)*60 + 20*rand.Float32(), -20, 600 + float32(j)*60 + 20*rand.Float32()}
 			temp := rand.Float32()
 			if temp < 0.8 {
-				world.Add(NewSphere(pos, 20, NewSolidLambert(RandClr())))
+				pos2 := pos.Add(mgl32.Vec3{0, -rand.Float32() * 40, 0})
+				world.Add(NewMoveSphere(pos, pos2, 20, NewSolidLambert(RandClr())))
 			} else if temp < 0.95 {
 				world.Add(NewSphere(pos, 20, NewMetal(RandVec(0.5, 1), Rand(0, 0.5))))
 			} else {
