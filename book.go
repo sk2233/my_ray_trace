0faa36a76547b88ab4c26a7b24bd42fe3c9f1bbe
shaokang.sk <shaokang@wepie.com>
2025-09-27 20:46:38 +0800

    渲染康奈盒

--- book.go
+++ book.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math"
 	"math/rand/v2"
 
 	"github.com/go-gl/mathgl/mgl32"
@@ -24,32 +25,27 @@ func TestBook() {
 	angle := mgl32.DegToRad(45)
 	// 相机距离视口的距离
 	l := w / 2 / Tan(angle)
-	center := mgl32.Vec3{0, -100, 0} // 相机位置
+	center := mgl32.Vec3{0, 0, 0} // 相机位置
 	rotate := mgl32.HomogRotate3DX(mgl32.DegToRad(0)).
 		Mul4(mgl32.HomogRotate3DY(mgl32.DegToRad(0))) // 相机旋转 x y 是反的
 	// 世界设置
-	background = mgl32.Vec3{0.7, 0.8, 1}
-	// 设置地面
-	chess := NewChessTexture(10, NewSolidColor(mgl32.Vec3{0.2, 0.3, 0.1}), NewSolidColor(mgl32.Vec3{0.9, 0.9, 0.9}))
-	world.Add(NewSphere(mgl32.Vec3{0, 10000, 600}, 10000, NewLambert(chess)))
-	tex := NewImageTexture(LoadImage("res/test2.png"))
-	// 设置其他场景
-	for i := -8; i < 8; i++ {
-		for j := -5; j < 5; j++ {
-			pos := mgl32.Vec3{float32(i)*60 + 20*rand.Float32(), -20, 600 + float32(j)*60 + 20*rand.Float32()}
-			temp := rand.Float32()
-			if temp < 0.8 {
-				world.Add(NewSphere(pos, 20, NewSolidLambert(RandClr())))
-			} else if temp < 0.95 {
-				world.Add(NewSphere(pos, 20, NewMetal(RandVec(0.5, 1), Rand(0, 0.5))))
-			} else {
-				world.Add(NewSphere(pos, 20, NewDielectric(1.5)))
-			}
-		}
+	background = mgl32.Vec3{0, 0, 0}
+	red := NewSolidLambert(mgl32.Vec3{0.65, 0.05, 0.05})
+	white := NewSolidLambert(mgl32.Vec3{0.73, 0.73, 0.73})
+	green := NewSolidLambert(mgl32.Vec3{0.12, 0.45, 0.15})
+	light := NewSolidLight(mgl32.Vec3{15, 15, 15})
+	world.Add(NewQuad(mgl32.Vec3{-300, -300, 700 + 200}, mgl32.Vec3{0, 0, -400}, mgl32.Vec3{600, 0, 0}, white))
+	world.Add(NewQuad(mgl32.Vec3{-300, -300, 700 + 200}, mgl32.Vec3{600, 0, 0}, mgl32.Vec3{0, 600, 0}, white))
+	world.Add(NewQuad(mgl32.Vec3{-300, -300, 700 + 200}, mgl32.Vec3{0, 600, 0}, mgl32.Vec3{0, 0, -400}, green))
+	world.Add(NewQuad(mgl32.Vec3{300, 300, 700 - 200}, mgl32.Vec3{-600, 0, 0}, mgl32.Vec3{0, 0, 400}, white))
+	world.Add(NewQuad(mgl32.Vec3{300, 300, 700 - 200}, mgl32.Vec3{0, 0, 400}, mgl32.Vec3{0, -600, 0}, red))
+	world.Add(NewQuad(mgl32.Vec3{-100, -300, 700 + 100}, mgl32.Vec3{0, 0, -200}, mgl32.Vec3{200, 0, 0}, light))
+	// 两个旋转的方块
+	quads := NewBox(mgl32.Vec3{100, 400, 100}, mgl32.Vec3{-200, -100, 700 - 100}, mgl32.Vec3{0, math.Pi / 12, 0}, white)
+	quads = append(quads, NewBox(mgl32.Vec3{100, 200, 100}, mgl32.Vec3{100, 100, 700 - 100}, mgl32.Vec3{0, -math.Pi / 12, 0}, white)...)
+	for _, quad := range quads {
+		world.Add(quad)
 	}
-	world.Add(NewSphere(mgl32.Vec3{0, -100, 600}, 100, NewDielectric(1.5)))
-	world.Add(NewSphere(mgl32.Vec3{-200, -100, 600}, 100, NewLambert(tex)))
-	world.Add(NewSphere(mgl32.Vec3{200, -100, 600}, 100, NewMetal(mgl32.Vec3{0.7, 0.6, 0.5}, 0)))
 	// 进行渲染
 	img := image.NewRGBA(image.Rect(0, 0, int(w), int(h)))
 	total := int(w) * int(h)
